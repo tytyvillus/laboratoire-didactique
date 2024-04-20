031ace129efca53752ea1d7489aacaae79ce91ed
Colife007 <167411889+Colife007@users.noreply.github.com>
2024-04-20 14:50:04 +0200

    Update codeSysteme.lua

--- equations-systeme/codeSysteme.lua
+++ equations-systeme/codeSysteme.lua
@@ -17,6 +17,15 @@ math.randomseed(os.time()) -- e.g. 2 or os.time()
 -- Set probability to generate singular matrices (number between 0 and 1)
 probability_singular = 0.2 -- here 20% of the generated singular equations systems will be kept (the real proportion is way less, since the probability of generating a singular matrix fully at random approaches 0)
 
+local function whether_singular () 
+    -- randomly decides whether choose top have a unique solution first (return true)
+    -- or generate a singular system (return false)
+    if math.random(10)/10 <= probability_singular --> weightings, feel free to change
+        then return true 
+        else return false 
+    end 
+end
+
 -- HELPER FUNCTIONS --
 
 local function is_one_of (value, table)
@@ -86,15 +95,15 @@ function solveLinearEquations(system)
     if determinant == 0 then
         print("The lines are either overlapping or parallel.")
         return nil
-    end
-    
-    -- Calculate x and y using Cramer's rule
-    local x = (b2 * c1 - b1 * c2) / determinant
-    local y = (a1 * c2 - a2 * c1) / determinant
+    else    
+        -- Calculate x and y using Cramer's rule
+        local x = (b2 * c1 - b1 * c2) / determinant
+        local y = (a1 * c2 - a2 * c1) / determinant
 
-    -- local x_num, x_den = decimalToFraction(x)
-    -- local y_num, y_den = decimalToFraction(y)
-    return x, y
+        -- local x_num, x_den = decimalToFraction(x)
+        -- local y_num, y_den = decimalToFraction(y)
+        return x, y
+    end
 end
 
 
@@ -104,29 +113,62 @@ end
 
 -- Generate two factors linear polynomials:
  -- generate 2x2 matrices to have 0, 1 or infinitely many solutions depending on the random number given (if the matrix is singular, then it will be wiped away 80% of the time)
-local function generate_matrix_two(probability_singular)
-    --- returns {a, b, d, e}
-    -- Generate random values for the matrix
-    local a = math.random(-10, 10)
-    local b = math.random(-10, 10)
-    local d = math.random(-10, 10)
-    local e = math.random(-10, 10)
+local function generate_system(probability_singular)
     
-    -- Check if the determinant is non-zero
-    local determinant = a * e - b * d
+    local rat = whether_singular()
 
-    -- Determine if the matrix should be singular or invertible based on probability
-    local is_singular = math.random() < probability_singular
+    if rat == true then -- integer solutions
+        if math.random()<= 0.5 then
+            local coef_bdy = 12 -- boundary of coefficient range
+
+            -- generate first equation
+            local a1 = math.random(-coef_bdy, coef_bdy)
+            local b1 = math.random(-coef_bdy, coef_bdy)
+            local c1 = math.random(-coef_bdy, coef_bdy)
+
+            -- also generate proportionality coefficient:
+            local k = math.random(-7, 7)
+            -- now compute other coefficients:
+            local a2, b2, c2 = k*a1, k*b1, k*c1
+            system = {a1, b1, c1, a2, b2, c2}
+            return system
+        else
+            local coef_bdy = 12 -- boundary of coefficient range
+
+            -- generate first equation
+            local a1 = math.random(-coef_bdy, coef_bdy)
+            local b1 = math.random(-coef_bdy, coef_bdy)
+            local c1 = math.random(-coef_bdy, coef_bdy)
+
+            -- also generate proportionality coefficient:
+            local k = math.random(-7, 7)
+            -- now compute other coefficients:
+            local a2, b2, c2 = k*a1, k*b1, c1
+
+            system = {a1, b1, c1, a2, b2, c2}
+
+            return system
+        end
+
+    else -- Generate random values for the matrix
+        local a1 = math.random(-10, 10)
+        local b1 = math.random(-10, 10)
+        local a2 = math.random(-10, 10)
+        local b2 = math.random(-10, 10)
         
-    if is_singular and determinant==0 then
-         -- If matrix should be singular and is singular, return singular matrix
-        return {{a, b}, {d, e}}
-
-    elseif determinant==0 then 
-        return generate_matrix_two(probability_singular)
-    else 
-        -- Return the matrix
-        return {{a, b}, {d, e}}
+        -- Choose arbitrary constants for the linear equations
+        local c1 = math.random(-15, 15)
+        local c2 = math.random(-15, 15)
+        local system = {a1, b1, c1, a2, b2, c2}
+        -- Check if the determinant is non-zero
+        local determinant = a1 * b2 - b1 * a1
+        
+        if determinant==0 then 
+            return generate_system(probability_singular)
+        else 
+            -- Return the system
+            return system
+        end
     end
 end
 
@@ -152,22 +194,27 @@ local function number_solutions_two(eqs)
     local determinant = a1 * b2 - a2 * b1
 
     -- Check if the lines are coincident
-    if a1/a2 == b1/b2 and b1/b2 == c1/c2 then
-        num_sols = math.huge -- Coincident lines, infinitely many solutions
-    
-    -- Check if the lines are parallel
-    elseif a1/a2 == b1/b2 and a1/a2 ~= c1/c2 then
-        num_sols= 0 -- Parallel lines, no solutions
-
-    else num_sols=1 -- lines are intersecting
-
-    return num_sols
+    if a2~=0 and b2~=0 and c2~=0 then
+        if a1/a2 == b1/b2 and b1/b2 == c1/c2 then
+            return math.huge -- Coincident lines, infinitely many solutions
+        -- Check if the lines are parallel
+        elseif a1/a2 == b1/b2 and a1/a2 ~= c1/c2 then
+            return 0 -- Parallel lines, no solutions
+        else  return 1 -- lines are intersecting
+        end
+    elseif a1~=0 and b1~=0 and c1~=0 then
+        if a2/a1 == b2/b1 and b2/b1 == c2/c1 then
+            return math.huge -- Coincident lines, infinitely many solutions
+        -- Check if the lines are parallel
+        elseif a2/a1 == b2/b1 and a2/a1 ~= c2/c1 then
+            return 0 -- Parallel lines, no solutions
+        else  return 1 -- lines are intersecting
+        end
     end
 end
 
 local function generate_exercise(probability_singular)
-    local matrix = generate_matrix_two(probability_singular)
-    local system = generate_linear_system(matrix)
+    local system = generate_system(probability_singular)
     local num_sols = number_solutions_two(system)
     local x, y = solveLinearEquations(system)
     
@@ -192,13 +239,13 @@ local function pick_method_case(system, num_sols)
         if a==0 or b==1 then method = [[isoler y dans la première équation, substituer ensuite]]
         elseif b==0 or a==1 then method = [[isoler x dans la première équation, substituer ensuite]]
         elseif d==0 or e==1 then method = [[isoler y dans la deuxième équation, substituer ensuite]]
-        elseif e==0 or d==1 then method = [[isoler x dans la première équation, substituer ensuite]]
+        elseif e==0 or d==1 then method = [[isoler x dans la deuxième équation, substituer ensuite]]
         elseif a==d or b==e then method = [[soustraction directe des deux équations]]
         elseif a==-d or b==-e then method = [[addition directe des deux équations]]
-        elseif a%b==0 then method = string.format([[diviser par \(%d\) puis isoler x dans la première équation, substituer ensuite]], b)
-        elseif b%a==0 then method = string.format([[diviser par \(%d\) puis isoler y dans la première équation, substituer ensuite]], a)
-        elseif d%e==0 then method = string.format([[diviser par \(%d\) puis isoler x dans la deuxième équation, substituer ensuite]], e)
-        elseif e%d==0 then method = string.format([[diviser par \(%d\) puis isoler y dans la deuxième équation, substituer ensuite]], d)
+        elseif a%b==0 then method = string.format([[diviser par \(%d\) puis isoler y dans la première équation, substituer ensuite]], b)
+        elseif b%a==0 then method = string.format([[diviser par \(%d\) puis isoler x dans la première équation, substituer ensuite]], a)
+        elseif d%e==0 then method = string.format([[diviser par \(%d\) puis isoler y dans la deuxième équation, substituer ensuite]], e)
+        elseif e%d==0 then method = string.format([[diviser par \(%d\) puis isoler x dans la deuxième équation, substituer ensuite]], d)
         else method = [[par combinaison linéaire]]
         end
 
@@ -271,7 +318,7 @@ local function cas_sol_set (system, num_sols, x, y)
             x = (%d * %d - %d * %d) / determinant
             y = (%d * %d - %d * %d) / determinant
             \end{CAS}
-            \left\{\print*{x}, \print*{y} \right\}
+            \left\{\left(\print*{x}, \print*{y} \right)\right\}
         ]], -- droites sécantes
         system[1], system[5], system[4], system[2], system[5], system[3], system[2], system[6], system[1], system[6], system[4], system[3]
     )
